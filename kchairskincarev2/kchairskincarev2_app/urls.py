2db6ca315a1be621f8a021d4cc2ba5a5d77719f7
Cruz <Onel.Cruz@G3TI.Local>
2024-10-07 22:41:31 -0400

    More initial changes for the website, everything still baseline

--- kchairskincarev2/kchairskincarev2_app/urls.py
+++ kchairskincarev2/kchairskincarev2_app/urls.py
@@ -4,4 +4,18 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('team/', views.team, name='team'),
+    path('post/', views.post, name='post'),
+    path('faq/', views.faq, name='faq'),
+    path('services-page/', views.services_page, name='services_page'),
+    path('team-details/', views.team_details, name='team_details'),
+    path('error-404/', views.error_404, name='error_404'),
+    path('coming-soon/', views.coming_soon, name='coming_soon'),
+    path('blog/', views.blog, name='blog'),
+    path('blog2/', views.blog2, name='blog2'),
+    path('blog3/', views.blog3, name='blog3'),
+    path('contact/', views.contact, name='contact'),
 ]
--- kchairskincarev2/kchairskincarev2_app/views.py
+++ kchairskincarev2/kchairskincarev2_app/views.py
@@ -9,3 +9,59 @@ def home(request):
 def about(request):
     context = {}
     return render(request, 'about.html', context)
+
+def services(request):
+    context = {}
+    return render(request, 'services.html', context)
+
+def pricing(request):
+    context = {}
+    return render(request, 'pricing.html', context)
+
+def portfolio(request):
+    context = {}
+    return render(request, 'portfolio.html', context)
+
+def team(request):
+    context = {}
+    return render(request, 'team.html', context)
+
+def faq(request):
+    context = {}
+    return render(request, 'faq.html', context)
+
+def services_page(request):
+    context = {}
+    return render(request, 'services-page.html', context)
+
+def team_details(request):
+    context = {}
+    return render(request, 'team-details.html', context)
+
+def post(request):
+    context = {}
+    return render(request, 'post.html', context)
+
+def error_404(request):
+    context = {}
+    return render(request, '404.html', context)
+
+def coming_soon(request):
+    context = {}
+    return render(request, 'coming-soon.html', context)
+
+def blog(request):
+    context = {}
+    return render(request, 'blog.html', context)
+
+def blog2(request):
+    context = {}
+    return render(request, 'blog2.html', context)
+
+def blog3(request):
+    context = {}
+    return render(request, 'blog3.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'contact.html', context)
